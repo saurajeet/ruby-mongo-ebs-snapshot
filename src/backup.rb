cb2de25d43522eab984baa295a4f55fa25ce014f
Saurajeet Dutta <vention.gothics@gmail.com>
2013-08-13 13:49:41 +0000

    + Mongo Reachability

--- src/backup.rb
+++ src/backup.rb
@@ -7,8 +7,10 @@ require 'pp'
 require 'curb'
 require 'yaml'
 
+include Mongo
+
 class Backup
-	attr_accessor :instance_id, :volume_id, :infra, :device, :settings
+	attr_accessor :instance_id, :volume_id, :infra, :device, :settings, :mongo_client
 	def initialize
 		@settings = YAML.load_file "conf/settings.yml"
 		AWS.config({
@@ -18,6 +20,15 @@ class Backup
 		})
 		@device = @settings[:device.to_s]
 		@infra = AWS.ec2
+	
+		begin	
+			@mongo_client = MongoClient.new 'localhost', @settings[:mongo_port.to_s].to_i
+			@mongo_client.add_auth "admin", @settings[:admin_user.to_s], @settings[:admin_pass.to_s], nil
+			@mongo_client.apply_saved_authentication
+		rescue
+			puts "Error in connectivity with Mongo, please set the mongo admin credentials in conf/settings.yml"
+			@mongo_client = nil
+		end
 		
 		url = "http://169.254.169.254/latest/meta-data/instance-id"
 		response = Curl::Easy.perform(url)
@@ -25,21 +36,26 @@ class Backup
 	end
 
 	def mongo_fs_lock
+		return if (@mongo_client == nil)
+		@mongo_client.lock!
 	end
 
 	def mongo_fs_unlock
+		return if (@mongo_client == nil)
+		@mongo_client.unlock!
 	end
 
 	def list_snapshots
 	end
 
 	def backup_vol
+		return if (@mongo_client == nil)
 		@volume_id = ""
 		@infra.instances[@instance_id].block_devices.each do |map|
 			next if (map[:device_name] != @device)
-			vol_attr = map[:ebs]
-			puts "Device doesnot exist" if vol_attr == nil
-			puts "A live Volume should be attached and recording" if (map[:ebs][:status] != "attached")
+			vol_attr = map[:ebs] 
+			puts "Device doesnot exist" & return if vol_attr == nil
+			puts "A live Volume should be attached and recording" & return if (map[:ebs][:status] != "attached")
 			@volume_id = map[:ebs][:volume_id]
 			puts "Snapshotting Started on #{@volume_id}"
 		end
