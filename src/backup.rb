2be1824a8e2f0169a51c8cc11784078a010b0397
Saurajeet Dutta <vention.gothics@gmail.com>
2013-08-16 13:20:12 +0000

    +MongoRSclient for master status

--- src/backup.rb
+++ src/backup.rb
@@ -10,7 +10,7 @@ require 'yaml'
 include Mongo
 
 class Backup
-	attr_accessor :instance_id, :volume_id, :infra, :device, :settings, :mongo_client
+	attr_accessor :instance_id, :volume_id, :infra, :device, :settings, :mongo_client, :mongo_node, :mongo_rs_client
 	def initialize
 		@settings = YAML.load_file "conf/settings.yml"
 		AWS.config({
@@ -29,6 +29,10 @@ class Backup
 			puts "Error in connectivity with Mongo, please set the mongo admin credentials in conf/settings.yml"
 			@mongo_client = nil
 		end
+		rs_seed = "localhost:" + @settings[:mongo_port.to_s].to_s
+		@mongo_rs_client = MongoReplicaSetClient.new([rs_seed])
+		@mongo_node = Mongo::Node.new @mongo_rs_client, @settings[:mongo_port.to_s].to_i
+		puts @mongo_client.check_is_master "mongodb://localhost:27017"
 		
 		url = "http://169.254.169.254/latest/meta-data/instance-id"
 		response = Curl::Easy.perform(url)
@@ -54,8 +58,8 @@ class Backup
 		@infra.instances[@instance_id].block_devices.each do |map|
 			next if (map[:device_name] != @device)
 			vol_attr = map[:ebs] 
-			puts "Device doesnot exist" & return if vol_attr == nil
-			puts "A live Volume should be attached and recording" & return if (map[:ebs][:status] != "attached")
+			puts "Device doesnot exist" if (vol_attr == nil)
+			puts "A live Volume should be attached and recording" if (map[:ebs][:status] != "attached")
 			@volume_id = map[:ebs][:volume_id]
 			puts "Snapshotting Started on #{@volume_id}"
 		end
